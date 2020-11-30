073c82d7eb1cf7dcabeebea146b81d82097d5561
flo62134 <florentbrassart31@gmail.com>
2020-11-30 18:28:37 +0100

    Added unit tests

--- tests/Calculator/CalculatorTest.php
+++ tests/Calculator/CalculatorTest.php
@@ -16,27 +16,21 @@ class CalculatorTest extends TestCase
         $this->source = new Calculator();
     }
 
-    // public function test_it_adds_2_numbers(): void
-    // {
-    //     $result = $this->source->add(1, 2);
-    //     self::assertEquals((float) 3, $result);
-    // }
+    public function testAdd(): void
+    {
+        $result = $this->source->add(1, 2);
+        self::assertEquals(3, $result);
+    }
 
-    // public function testIsPositive(): void
-    // {
-    //     $result = $this->source->isNegative(3);
-    //     self::assertFalse($result);
-    // }
+    public function testIsNegative(): void
+    {
+        $result = $this->source->isNegative(-2);
+        self::assertTrue($result);
+    }
 
-    // public function testIsNegative(): void
-    // {
-    //     $result = $this->source->isNegative(-2);
-    //     self::assertTrue($result);
-    // }
-
-    // public function testZeroIsNegative(): void
-    // {
-    //     $result = $this->source->isNegative(0);
-    //     self::assertFalse($result);
-    // }
+    public function testZeroIsNegative(): void
+    {
+        $result = $this->source->isNegative(0);
+        self::assertFalse($result);
+    }
 }
